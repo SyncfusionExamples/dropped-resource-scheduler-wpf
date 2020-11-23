405fd8418065da64e01f18d72e26769b515b5fc6
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-23 16:21:31 +0530

    WPF-55689 - Sample and screenshot added

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+using System.Windows;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<SfScheduler>
+    {
+        SfScheduler scheduler;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            scheduler = this.AssociatedObject;
+            this.AssociatedObject.AppointmentDropping += AssociatedObject_AppointmentDropping;
+        }
+        private void AssociatedObject_AppointmentDropping(object sender, AppointmentDroppingEventArgs e)
+        {
+            var dropitem = e.TargetResource.Name;
+            MessageBox.Show("Resource change into" + " " + dropitem, " ", MessageBoxButton.OK);
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.AppointmentDropping -= AssociatedObject_AppointmentDropping;
+            this.scheduler = null;
+        }
+    }
+}
